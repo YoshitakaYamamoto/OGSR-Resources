cdf05581b8fdff2f465441493bee6aba8a397f82
KRodinn <stalker.moderator@gmail.com>
2016-08-21 22:14:34 +0300

    Удалил схему убегания от пси-зон
    Т.к. пси-зон больше нет, да и схема эта не работала

--- gamedata/scripts/personal_test.lua
+++ gamedata/scripts/personal_test.lua
@@ -13,3 +13,21 @@ log1(rbt:lower())
 log1(emt:upper())
 log1(rmt:upper())
 --]]---------------------------------------------------------
+
+--[[---------------------------------------------------------
+for i = 1, 65534 do
+	local sobj = alife():object(i)
+	if sobj then
+		if sobj:clsid() == clsid.respawn then
+			local pk = xs_netpk.net_respawn(sobj)
+			if not pk:isOk() then log3("!!Error!!! PK Isn't OK fot [%s]", sobj:name()) return end
+			local data = pk:get()
+			log4("[Respawners]", "[Data of %s]: %s", sobj:name(), data)
+			local cd   = data.custom_data:getTable()
+			log4("[Respawners]", "Customdata: %s", cd)
+		end
+	end
+end
+
+dsh_respawn.cleanup_respawners()
+]]-----------------------------------------------------------
