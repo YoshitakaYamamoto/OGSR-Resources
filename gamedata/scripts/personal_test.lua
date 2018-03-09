7996b93f1db88eb75ec87fba299e604bb312189b
KRodin <stalker.moderator@gmail.com>
2018-03-09 23:22:49 +0300

    Поправил ошибки

--- gamedata/scripts/personal_test.lua
+++ gamedata/scripts/personal_test.lua
@@ -13,3 +13,20 @@ level.add_call(function() return true end, function() fail("err") end)
 
 --log1(debug.traceback())
 collectgarbage("collect")
+
+--Вывод на карту НПС, которых создали респавнеры.
+  dsh_respawn.iterate_respawners( function( respawner )
+    if not respawner.item_spawn and #respawner.spawned_obj > 0 then
+      for _, id in ipairs( respawner.spawned_obj ) do
+        local sobj = alife():object( id )
+        if sobj then
+          local smart_name = "NOT FOUND"
+          if sobj:smart_terrain_id() ~= 65535 then
+			local smart_sobj = alife():object(sobj:smart_terrain_id())
+			smart_name = smart_sobj:name()
+		  end
+          level.map_add_object_spot( sobj.id, "red_location", sobj:name()..": respawner ["..respawner.name.."] smart ["..smart_name.."]" )
+        end
+      end
+    end
+  end )
